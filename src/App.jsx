42ee84fb820f8d5101d4d28ca1279a44c64bdcbf
zozoo89 <zowangstudent@shacademy.org>
2024-09-17 16:28:31 +0000

    Zoe Wang 9/17

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,8 @@ function App() {
       <header className="App-header">
         <img src="Octocat.png" className="App-logo" alt="logo" />
         <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
+          GitHub Codespaces <span className="heart">♥️</span> Olive
+        
         </p>
         <p className="small">
           Edit <code>src/App.jsx</code> and save to reload.
